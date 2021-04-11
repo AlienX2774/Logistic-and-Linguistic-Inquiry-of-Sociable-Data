4ba668429669cec28ccd9f5c2cd13468be81a843
AlienX2774 <82318678+AlienX2774@users.noreply.github.com>
2021-04-11 17:48:08 +0530

    Add files via upload

---
+++ nlp.py
@@ -0,0 +1,20 @@
+import re
+from nltk.stem import PorterStemmer
+import string
+from nltk.corpus import stopwords
+import pandas as pd
+import nltk
+tweets = pd.read_csv(r'C:\Users\saura\Downloads\Logistic-and-Linguistic-Inquiry-of-Socialable-Data-main\file_name.csv')
+X=tweets['Text']
+nltk.download('stopwords')
+stop_words=stopwords.words('english')
+punct=string.punctuation
+stemmer=PorterStemmer()
+cleaned_data=[]
+for i in range(len(X)):
+    tweet=re.sub('[^a-zA-Z]', ' ', X.iloc[i])
+    tweet=tweet.lower().split()
+    tweet=[stemmer.stem(word) for word in tweet if (word not in stop_words) and (word not in punct)]
+    tweet=' '.join(tweet)
+    cleaned_data.append(tweet)
+print(cleaned_data)
