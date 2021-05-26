c288f1174a695780bff9dc16591a328c3b92ab69
Hartej Singh <55663784+hartej-singh@users.noreply.github.com>
2021-05-27 01:57:47 +0530

    Added graphs

--- 5_potential_data.py
+++ 5_potential_data.py
@@ -4,7 +4,6 @@ import nltk
 from pprint import pprint
 from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk.text import Text
-#import spacy
 import re
 from nltk.stem import PorterStemmer
 import string
@@ -64,7 +63,73 @@ AP=pd.DataFrame(All_Pos)
 AN.to_csv(r'E:\VIT\Semester-4\CSE4022 (NLP)\Project\Codes\Dataset\trained_neg.csv')
 AP.to_csv(r'E:\VIT\Semester-4\CSE4022 (NLP)\Project\Codes\Dataset\trained_pos.csv')
 
+pos_date1 = pd.read_csv(r'E:\VIT\Semester-4\CSE4022 (NLP)\Project\Codes\Dataset\positive_data.csv')
+neg_date1 = pd.read_csv(r'E:\VIT\Semester-4\CSE4022 (NLP)\Project\Codes\Dataset\negative_data.csv')
+pos_date = pos_date1['Datetime']
+neg_date = neg_date1['Datetime']
+pc_2014=0
+pc_2015=0
+pc_2016=0
+pc_2017=0
+pc_2018=0
+pc_2019=0
+pc_2020=0
+pc_2021=0
+
+nc_2014=0
+nc_2015=0
+nc_2016=0
+nc_2017=0
+nc_2018=0
+nc_2019=0
+nc_2020=0
+nc_2021=0
+for i in pos_date:
+    if i.startswith("2014"):
+        pc_2014 = pc_2014+1
+    elif i.startswith("2015"):
+        pc_2015 = pc_2015+1
+    elif i.startswith("2016"):
+        pc_2016 = pc_2016+1
+    elif i.startswith("2017"):
+        pc_2017 = pc_2017+1
+    elif i.startswith("2018"):
+        pc_2018 = pc_2018+1
+    elif i.startswith("2019"):
+        pc_2019 = pc_2019+1
+    elif i.startswith("2020"):
+        pc_2020 = pc_2020+1
+    elif i.startswith("2021"):
+        pc_2021 = pc_2021+1
+
+for i in neg_date:
+    if i.startswith("2014"):
+        nc_2014 = nc_2014+1
+    elif i.startswith("2015"):
+        nc_2015 = nc_2015+1
+    elif i.startswith("2016"):
+        nc_2016 = nc_2016+1
+    elif i.startswith("2017"):
+        nc_2017 = nc_2017+1
+    elif i.startswith("2018"):
+        nc_2018 = nc_2018+1
+    elif i.startswith("2019"):
+        nc_2019 = nc_2019+1
+    elif i.startswith("2020"):
+        nc_2020 = nc_2020+1
+    elif i.startswith("2021"):
+        nc_2021 = nc_2021+1
+p_allcounts = [pc_2014, pc_2015, pc_2016, pc_2017, pc_2018, pc_2019, pc_2020, pc_2021]
+n_allcounts =[nc_2014, nc_2015, nc_2016, nc_2017, nc_2018, nc_2019, nc_2020, nc_2021]
+years = ["2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021"]
+plt.subplot(2,2,3)
+plt.plot(years, p_allcounts)
+plt.subplot(2,2,4)
+plt.plot(years, n_allcounts)
+
+
 y=nump.array([ncount,pcount])
 ml=["Negative","Positive"]
+plt.subplot(2,2,1)
 plt.pie(y, labels = ml,autopct='%1.2f%%')
 plt.show()
